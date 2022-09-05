07f67da7c50977655b914a815dea9ec360dfb31c
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-09-05 16:35:43 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -2,28 +2,28 @@ let fetch = require('node-fetch')
 let teks = 'Donasi'
 let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch('https://telegra.ph/file/c6d47e0aa9dca2263bee9.png'+ teks)).buffer(), `
 
-*𝐇𝐚𝐥𝐨 𝐁𝐚𝐧𝐡 𝐃𝐨𝐧𝐚𝐬𝐢 𝐲𝐮𝐤 𝐁𝐢𝐚𝐫 𝐁𝐨𝐭 𝐀𝐤𝐭𝐢𝐟 𝐒𝐞𝐥𝐚𝐥𝐮👋*
+*𝐇i 𝐁𝐚𝐧𝐡 𝐃𝐨𝐧𝐚𝐬𝐢 𝐲𝐮𝐤 𝐁𝐢𝐚𝐫 𝐁𝐨𝐭 𝐀𝐤𝐭𝐢𝐟 𝐒𝐞𝐥𝐚𝐥𝐮👋*
 ╔═══════════════════
-║ _*DONASI UNTUK*_  𝐉𝐚𝐫𝐨𝐭 ×͜×
+║ _*DONASI UNTUK*_  Owner baik
 ╠═══════════════════
 ║          ❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉
-║➸ *DANA* : 
-║➸ 085850539404
-║➸ *PULSA
-║➸ 085850539404
-║➸ *Saweria*:
-║ https://saweria.co/jarotoffc
+║-> *DANA* : 
+║> CHT OWNER 
+║-> *GOPAY*
+║> CHT OWNER 
+║-> *SHOPPAY*:
+║> CHT OWNER 
 ╰═══════════════════
 ╔════════════════════
-║ *Donasi Via Ikuti Sosial media*
+║ *Donasi Via Ikuti Sosial media YOK*
 ╠════════════════════
-║     ❉ 〔 𝐒𝐨𝐬𝐢𝐚𝐥 𝐌𝐞𝐝𝐢𝐚 𝐉𝐚𝐫𝐨𝐭 〕 ❉
-║➸ *Instasgram*
-║ https://instagram.com/Jarotr_
-║➸ *Youtube*
-║ https://tinyurl.com/2qsejxnw
+║     >> 〔 SOOCIAL MEDIAK OWNER 〕 <<
+║➸ *Instagram*
+║ Ga main ige aslinya
+║➸ *Grub Bot*
+║ https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz
 ║➸ *Github*
-║ https://github.com/Alpiii22 
+║ SC CARI SENDIRI DEK
 ╚════════════════════
 
 `.trim(), wm, 'Owner', '.owner', 'Menu', '.menu')
