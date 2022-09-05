277a3546f0bc3444b4d60d7344f7c1cf43293b89
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-09-05 16:28:57 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,16 +3,16 @@ let handler = async (m, {conn}) => {
 const vcard = `BEGIN:VCARD
 VERSION:3.0
 N:;;;
-FN: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item.ORG: ᴹᴿ᭄ OscarOffcོ_1ঔৣ꧂
-item1.TEL;waid=6285850539404:6285850539404@s.whatsapp.net
-item1.X-ABLabel:owner selalu ada 
+FN: ᴹᴿ Unknown ツ
+item.ORG: ᴹᴿ Unknown ツ 
+item1.TEL;waid=6283805685278:6283805685278@s.whatsapp.net
+item1.X-ABLabel:Owner agak sibuk jadi inti aja
 item2.YOUTUBE;type=INTERNET: https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ
 item2.X-ABLabel:YOUTUBE
 item3.ADR:;;🎍AMERIKA SERIKAT;;;;
 item3.X-ABADR:ac
 item3.X-ABLabel:asal kota
-item4.URL:https//github.com/Alpiii22 
+item4.URL:https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz
 item4.X-ABLabel:Website
 END:VCARD`
 const sentMsg  = await conn.sendMessage(
@@ -25,7 +25,7 @@ const sentMsg  = await conn.sendMessage(
     }
 )
 let jarot = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg'
-await conn.send3ButtonImg(m.chat, jarot, `*Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor owner gw…*`, '📮: jngn di spam kack', '𝙎𝙚𝙬𝙖', '.sewa', '𝘽𝙖𝙘𝙠', '.menu', '𝘿𝙤𝙣𝙖𝙨𝙞', '.donasi', sentMsg)}
+await conn.send3ButtonImg(m.chat, SaxiaBot, `*✨Hai kak @${m.sender.split('@')[0]} 👋,  Itu nomor owner gwehj…*`, '📮: jngn di spam dek', '𝙎𝙚𝙬𝙖', '.sewa', '𝘽𝙖𝙘𝙠', '.menu', '𝘿𝙤𝙣𝙖𝙨𝙞', '.donasi', sentMsg)}
 handler.help = ['owner2', 'creator2']
 handler.tags = ['info']
 
