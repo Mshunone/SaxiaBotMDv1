1725f2d8f090dac0aa1924c8a5c73acae4f1ebdb
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-09-06 12:30:25 +0700

    Update sosialmedia.js

--- plugins/sosialmedia.js
+++ plugins/sosialmedia.js
@@ -1,7 +1,7 @@
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
 let handler = async (m) => {
-let jarot = `
+let Shunji = `
 ╭─❑ 「 *SOSIAL MEDIA* 」 ❑──
 │ Follow sosial media
 │ Owner kang nolep jadi seadanya
