bdabb5e4cf9a548619c65dcc68366039daae699a
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-09-06 12:05:04 +0700

    Update broadcastloc.js

--- plugins/broadcastloc.js
+++ plugins/broadcastloc.js
@@ -4,7 +4,7 @@ let handler  = async (m, { conn, text }) => {
   for (let id of chats) {
        let bcbg = 'https://telegra.ph/file/34c95be3feef20cb61dd5.jpg'
        await conn.delay(1500)
-       await conn.send2ButtonLoc(id, bcbg,'*——————「 Broadcast 」——————*\n' + text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', m)
+       await conn.send2ButtonLoc(id, bcbg,'*——————「 Broadcast Dek 」——————*\n' + text.trim(), wm, 'Menu', '.menu', 'Owner', '.owner', m)
      }
   m.reply('*Broadcast selesai*')
 }
