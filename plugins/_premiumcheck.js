6d37101c8e68623a76ba53439636af844319506f
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-09-09 20:08:31 +0700

    Update _premiumcheck.js

--- plugins/_premiumcheck.js
+++ plugins/_premiumcheck.js
@@ -6,10 +6,3 @@ handler.before = async (m, { conn, isPrems, owner }) => {
         global.db.data.users[m.sender].premium = false
         const data = global.owner.filter(([id, isCreator]) => id && isCreator)
          this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m).then(() => {
-    //       this.sendContact(m.chat, owner[99], this.getName(owner[99] + '@s.whatsapp.net'), m).then(() => {
-           })
-        })
-      }
-    }
- }
-module.exports = handler
