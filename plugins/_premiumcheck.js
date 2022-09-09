391c98416d77fd664d43e790f0d20f68abc02abd
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-09-09 20:05:12 +0700

    Update _premiumcheck.js

--- plugins/_premiumcheck.js
+++ plugins/_premiumcheck.js
@@ -2,11 +2,11 @@ let handler = m => m
 handler.before = async (m, { conn, isPrems, owner }) => {
 	if (isPrems) {
     if (new Date() * 1 >= global.db.data.users[m.sender].premiumDate) {
-      conn.reply(m.chat, "*[-Promosi-] Upgrade premium yok dek *\n*Chat owner untuk upgrade premium ya *", m).then(() => {
+      conn.reply(m.chat, "*[-Promosi-] Upgrade premium yok dek*\n*Chat owner untuk upgrade premium ya*", m).then(() => {
         global.db.data.users[m.sender].premium = false
         const data = global.owner.filter(([id, isCreator]) => id && isCreator)
          this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m).then(() => {
-    //       this.sendContact(m.chat, owner[0], this.getName(owner[0] + '@s.whatsapp.net'), m).then(() => {
+    //       this.sendContact(m.chat, owner[99], this.getName(owner[99] + '@s.whatsapp.net'), m).then(() => {
            })
         })
       }
